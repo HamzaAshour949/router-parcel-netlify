542613255870bb29753b07b1ad54abfbc315be14
HamzaAshour949 <hamza.ashour.work@gmail.com>
2023-07-21 11:12:50 +0300

    .

---
+++ src/components/main-view.jsx
@@ -0,0 +1,53 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { TestView } from "./test-view";
+
+function MainView() {
+  return (
+    <>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <TestView />
+                <ul>
+                  <li>
+                    <Link to={`/`}>homepagee</Link>
+                  </li>
+                  <li>
+                    <Link to={`/page2`}>page2</Link>
+                  </li>
+                  <li>
+                    <Link to={`/page3`}>page3</Link>
+                  </li>
+                </ul>
+                <div>this is the homepage at /</div>
+              </>
+            }
+          />
+          <Route
+            path="/page2"
+            element={
+              <>
+                <div>this is the page2 at /page2</div>
+                <Link to={`/`}>back</Link>
+              </>
+            }
+          />
+          <Route
+            path="/page3"
+            element={
+              <>
+                <div>this is the page3 at /page3</div>
+                <Link to={`/`}>back</Link>
+              </>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default MainView;
---
+++ src/components/test-view.jsx
@@ -0,0 +1,8 @@
+export const TestView = () => {
+  return (
+    <>
+      <div>this is the TEST component</div>
+      <img src={require("../assets/logo512.png")} />
+    </>
+  );
+};
